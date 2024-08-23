119ef3e7f1f66aac8b1438cdb024a5ca86356dde
guygubaby <guygubaby@gmail.com>
2024-08-23 09:54:21 +0800

    feat: add new useScrollView hook for uniapp

---
+++ src/uni/useScrollView.ts
@@ -0,0 +1,84 @@
+import { pMinDelay } from '@bryce-loskie/utils'
+import { ref } from 'vue'
+
+interface IOptions<T> {
+  onRefresh?: () => Promise<T> | T
+  enableToLower?: boolean
+  onScrollToLower?: () => Promise<T> | T
+}
+
+/**
+ * Usage
+ *
+ * @example
+ * ```typescript
+ * const { isRefreshingRef, refresherrefresh, scrollProps, scrolltolower } = useScrollViewMisc({
+ *     enableToLower: true,
+ *     onRefresh: async () => {
+ *       // refresh logic
+ *     },
+ *     onScrollToLower() {
+ *       // fetch more data
+ *     },
+ *   })
+ *
+ *   <scroll-view
+ *     scroll-y
+ *     v-bind="scrollProps"
+ *     :refresher-triggered="isRefreshingRef"
+ *     refresherrefresh="refresherrefresh" // 前面加 @ 会报错
+ *     scrolltolower="scrolltolower"  // 前面加 @ 会报错
+ *   >
+ *     <!-- your content -->
+ *   </scroll-view>
+ * ```
+ */
+export function useScrollViewMisc<T = any>(options: IOptions<T>) {
+  const isRefreshingRef = ref(false)
+  const isFetchingRef = ref(false)
+
+  const handleRefetch = () => {
+    return Promise.resolve(options.onRefresh?.())
+  }
+
+  const handleScrollToLower = () => {
+    return Promise.resolve(options.onScrollToLower?.())
+  }
+
+  const scrolltolower = async () => {
+    isFetchingRef.value = true
+    await pMinDelay(handleScrollToLower(), 500)
+    isFetchingRef.value = false
+  }
+
+  const refresherrefresh = async () => {
+    isRefreshingRef.value = true
+    await pMinDelay(handleRefetch(), 500)
+    isRefreshingRef.value = false
+  }
+
+  const scrollProps: Record<string, any> = {
+    enableBackToTop: true,
+    refresherEnabled: true,
+    refresherThreshold: 100,
+    showScrollbar: true,
+    refresherBackground: 'transparent',
+  }
+
+  if (options.enableToLower) {
+    scrollProps.lowerThreshold = 100
+  }
+  else {
+    delete scrollProps.lowerThreshold
+  }
+
+  return {
+    isRefreshingRef,
+    isFetchingRef,
+
+    scrollProps,
+
+    refresherrefresh,
+    scrolltolower,
+  }
+}
